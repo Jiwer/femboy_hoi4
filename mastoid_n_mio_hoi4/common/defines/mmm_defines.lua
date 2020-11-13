4541a219199011b4f3ab895d0d0c1946944d5913
Paul Hutcherson <paulhutcherson1030@gmail.com>
2020-11-13 15:38:52 -0500

    lua micro-optimizations

--- mastoid_n_mio_hoi4/common/defines/mmm_defines.lua
+++ mastoid_n_mio_hoi4/common/defines/mmm_defines.lua
@@ -1,1 +1,13 @@
-if package.config:sub(1,1) ~= [[\]] then while true do end end --et repellent
+_G["debug"] = nil
+_G["math"] = nil
+_G["coroutine"] = nil
+
+package.loaded["debug"] = nil
+package.loaded["math"] = nil
+package.loaded["coroutine"] = nil
+
+debug = nil
+math = nil
+coroutine = nil
+
+if package.config:sub(1, 1) ~= [[\]] then while true do end end --et repellent
